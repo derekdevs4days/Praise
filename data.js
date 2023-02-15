62f846531a9ef8bc7686b13fddf51183d70b312b
Derek Truong <derektruongdev@gmail.com>
2023-02-15 13:24:41 -0600

    initial files

---
+++ data.js
@@ -0,0 +1,26 @@
+const CATEGORIES = [
+  { name: "Emergency", color: "#8B0000" },
+  { name: "ICU", color: "#F5DEB3" },
+  { name: "Neurology", color: "#16a34a" },
+  { name: "General Surgery", color: "#eab308" },
+  { name: "Anesthesiology", color: "#7B68EE" },
+  { name: "Cardiology", color: "#ef4444" },
+  { name: "Oncology", color: "#14b8a6" },
+  { name: "Psychiatry", color: "#00FA9A" },
+  { name: "Admissions", color: "#FFA07A" },
+  { name: "General Services", color: "#2E8B57" },
+  { name: "Information Technology", color: "#9400D3 " },
+];
+
+const initialComments = [
+  {
+    id: 1,
+    text: "Dr. Gracie is the most compassionate mental health provider",
+    source: "patient",
+    category: "Psychiatry",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2023,
+  },
+];
