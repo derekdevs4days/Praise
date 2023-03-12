485bc7825963297d90f5fab813889dc56d496fe3
Derek Truong <derektruongdev@gmail.com>
2023-03-12 13:11:06 -0500

    v1.1 update

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env);
 const supabaseUrl = "https://jiwmiywskwvnsszhupxz.supabase.co";
-const supabaseKey = "";
+const supabaseKey = process.env.REACT_APP_SUPABASEKEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
